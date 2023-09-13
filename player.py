b8a805fd94c42fb1e81bff525b6d785260cf9078
Afiq <afiqroslin97@gmail.com>
2023-09-13 11:17:29 +0800

    initial commit

---
+++ player.py
@@ -0,0 +1,24 @@
+from turtle import Turtle
+STARTING_POSITION = (0, -280)
+MOVE_DISTANCE = 10
+FINISH_LINE_Y = 280
+
+
+class Player(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.spawn_turtle()
+
+    def spawn_turtle(self):
+        self.shape('turtle')
+        self.penup()
+        self.setheading(90)
+        self.goto(STARTING_POSITION)
+
+    def up(self):
+        self.forward(MOVE_DISTANCE)
+        # new_y = self.ycor() + 50
+        # self.goto(self.xcor(), new_y)
+        # print(new_y)
+
+
